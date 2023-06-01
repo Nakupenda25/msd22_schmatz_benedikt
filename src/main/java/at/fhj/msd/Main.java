a3d08d49b0393332874b57ff03e6fb42a02bd08d
benedikt.schmatz(at)gmail.com <benedikt.schmatz@edu.fh-joanneum.at>
2023-06-01 10:25:29 +0200

    Added first logs to program

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,8 +1,13 @@
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Logger logger = LogManager.getLogger();
         Calculator calculator = new Calculator();
         int number1 = 5;
         int number2 = 2;
@@ -10,9 +15,13 @@ public class Main {
         System.out.println("Number 1 = " + number1);
         System.out.println("Number 2 = " + number2 + "\n");
 
+        logger.info("This is an info message");
+
         System.out.println("Addition: " + calculator.add(number1, number2));
         System.out.println("Subtraction: " + calculator.minus(number1, number2));
         System.out.println("Division: " + calculator.divide(number1, number2));
         System.out.println("Multiplication: " + calculator.multiply(number1, number2));
+
+        logger.error("This is an error message");
     }
 }
