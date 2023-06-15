09bb5b92f6f2531643d8f2e09e7f8210a51fb141
benedikt.schmatz(at)gmail.com <benedikt.schmatz@edu.fh-joanneum.at>
2023-06-15 12:45:59 +0200

    Add Javadoc documentation for Calculator.java

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -3,19 +3,50 @@ package at.fhj.msd;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Calculator is the class that takes care of all the calculations. It has methods for addition, subtraction, division and multiplication.
+ * An instance of Calculator can be created to use the methods for calculating simple math problems.
+ * 
+ * @author Benedikt Schmatz
+ */
 public class Calculator {
     private static Logger logger = LogManager.getLogger("");
 
+    /**
+     * Adds two numbers together and returns the result.
+     *
+     * @param number1 First number to add
+     * @param number2 Second number to add
+     * @return the sum of the two numbers
+     * @author Benedikt Schmatz
+     */
     public double add(double number1, double number2){
         logger.debug("Addition called with: " + number1 + " and " + number2);
         return number1 + number2;
     }
 
+    /**
+     * Subtracts number2 from number1 and returns the result.
+     *
+     * @param number1 First number to subtract from
+     * @param number2 Second number to subtract
+     * @return the difference of the two numbers
+     * @author Benedikt Schmatz
+     */
     public double minus(double number1, double number2){
         logger.debug("Subtraction called with: " + number1 + " and " + number2);
         return number1 - number2;
     }
 
+    /**
+     * Divides number1 by number2 and returns the result.
+     *
+     * @param number1 The number that is to be divided
+     * @param number2 The factor by which the first number should be divided
+     * @return the quotient of the two numbers
+     * @throws ArithmeticException if number2 is 0, because division by zero is not allowed
+     * @author Benedikt Schmatz
+     */
     public double divide(double number1, double number2){
         logger.debug("Division called with: " + number1 + " and " + number2);
 
@@ -27,11 +58,27 @@ public class Calculator {
         return number1 / number2;
     }
 
+    /**
+     * Multiplies number1 by number2 and returns the result.
+     *
+     * @param number1 First number to multiply
+     * @param number2 Second number to multiply
+     * @return the product of the two numbers
+     * @author Beneidkt Schmatz
+     */
     public double multiply(double number1, double number2){
         logger.debug("Multiplication called with: " + number1 + " and " + number2);
         return number1 * number2;
     }
 
+    /**
+     * Calculates the factorial of a number and returns the result.
+     *
+     * @param value The number of which the factorial should be calculated
+     * @return the factorial of the number
+     * @throws ArithmeticException if the number is negative, because factorial of negative numbers is not allowed
+     * @author Benedikt Schmatz
+     */
     public double factorial(double value) {
         logger.debug("Factorial called with: " + value);
         if (value < 0) {
